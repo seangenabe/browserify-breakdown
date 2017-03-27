81ffc81df0329aa7361beceb0bad5076ed58b8c7
Sean Genabe <seangenabe@outlook.com>
2017-03-27 13:49:34 +0800

    add test for circular deps

---
+++ test/fixture2/b.js
@@ -0,0 +1,2 @@
+var b = require('./a.js')
+module.exports = 'b'
--- test/index.test.js
+++ test/index.test.js
@@ -25,10 +25,46 @@ test('breakdown', async t => {
     buffer => buffer.length
   )
 
-  t.is(result.deps[0].deps[1].deps[0].size, cz)
-  t.is(result.deps[0].deps[1].size, bz)
-  t.is(result.deps[0].deps[1].totalSize, bz + cz)
-  t.is(result.deps[0].deps[0].size, az)
-  t.is(result.deps[0].size, iz)
-  t.is(result.deps[0].totalSize, iz + az + bz + cz)
+  /*
+  bundle
+  - index
+    - a
+    - b
+      - c
+  */
+  const node_bundle = result.deps[0]
+  const node_a = node_bundle.deps[0]
+  const node_b = node_bundle.deps[1]
+  const node_c = node_b.deps[0]
+
+  t.is(node_c.size, cz)
+  t.is(node_b.size, bz)
+  t.is(node_b.totalSize, bz + cz)
+  t.is(node_a.size, az)
+  t.is(node_bundle.size, iz)
+  t.is(node_bundle.totalSize, iz + az + bz + cz)
+})
+
+test('circular dep', async t => {
+  let fdir = `${__dirname}/fixture2`
+  let stream = browserify(`${fdir}/index.js`, { fullPaths: true }).bundle()
+  let bundleSrc = await getStream(stream)
+  let result = breakdown(bundleSrc)
+
+  /*
+  bundle
+    - index
+      - a
+        - b
+          - a (CIRCULAR)
+  */
+
+  const node_bundle = result.deps[0]
+  const node_a = node_bundle.deps[0]
+  const node_b = node_a.deps[0]
+  const node_a_circular = node_b.deps[0]
+
+  t.is(node_a_circular.size, 0)
+  t.falsy(node_a_circular.deps)
+  t.true(node_a_circular.circular)
 })
